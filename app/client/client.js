5ba276e45f1c246d30dedc027f07850dae320ff4
perry <perry2008084@gmail.com>
2018-06-03 20:29:33 +0800

    添加基本页面

--- app/client/client.js
+++ app/client/client.js
@@ -5,10 +5,16 @@ angular.module('myApp.client', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/client', {
     templateUrl: 'client/client.html',
-    controller: 'ClientCtrl'
+    controller: 'ClientCtrl as vm'
   });
 }])
 
 .controller('ClientCtrl', [function() {
+  var vm = this;
 
+  vm.status = 'online';
+  vm.connectBtnText = '连接';
+  vm.input = {
+    longwillqos: 0
+  };
 }]);
